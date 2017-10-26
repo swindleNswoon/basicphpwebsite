e15df05ba1d36898be565c6962645ff229189bfe
Brian Lynch <bijil6339@gmail.com>
2017-10-26 06:43:58 -0400

    built media library, entered data into multidimensional array '

--- catalog.php
+++ catalog.php
@@ -1,4 +1,6 @@
 <?php 
+include("inc/data.php");
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -19,7 +21,19 @@ if (isset($_GET["cat"])) {
 
 include 'inc/header.php'; 
 ?>
-<div class="section page">
-	<h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+	<div class="wrapper">
+		<h1><?php echo $pageTitle; ?></h1>
+
+		<ul class="items">
+			<?php foreach ($catalog as $item) {
+				echo "<li><a href='#'><img src='"
+				 . $item["img"] . "' alt='"
+				 . $item["title"] . "'/>"
+				 . "<p>View Details</p>"
+				 . "</a></li>";
+			} ?>
+		</ul>
+	</div>
 </div>
 <?php include 'inc/footer.php'; ?>
